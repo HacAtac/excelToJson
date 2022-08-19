2136e32051d2e05ae8be50e4a81084644668eb67
HacAtac <jhackworth00@outlook.com>
2022-08-19 03:50:25 -0500

    update to const and let variables

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var XLSX = require("xlsx");
-var FS = require("fs");
-var workbook = XLSX.readFile("Data.xlsx");
-var sheet_name_list = workbook.SheetNames;
+const XLSX = require("xlsx");
+const FS = require("fs");
+const workbook = XLSX.readFile("Data.xlsx");
+const sheet_name_list = workbook.SheetNames;
 console.log(sheet_name_list); // getting as Sheet1
 
 sheet_name_list.forEach(function (y) {
-  var worksheet = workbook.Sheets[y];
+  let worksheet = workbook.Sheets[y];
   //getting the complete sheet
-  // console.log(worksheet);
+  console.log(worksheet);
 
-  var headers = {};
-  var data = [];
+  let headers = {};
+  let data = [];
   for (z in worksheet) {
     if (z[0] === "!") continue;
     //parse out the column, row, and value
-    var col = z.substring(0, 1);
-    // console.log(col);
+    let col = z.substring(0, 1);
+    console.log(col);
 
-    var row = parseInt(z.substring(1));
-    // console.log(row);
+    let row = parseInt(z.substring(1));
+    console.log(row);
 
-    var value = worksheet[z].v;
-    // console.log(value);
+    let value = worksheet[z].v;
+    console.log(value);
 
     //store header names
     if (row == 1) {
